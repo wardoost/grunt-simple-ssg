fcbba26acb8890e93ecbee35e507f3452defb1f6
Ward Oosterlijnck <ward_oosterlijnck@hotmail.com>
2015-12-03 21:00:47 +0100

    Color, font and animation changes

--- src/js/body/main.js
+++ src/js/body/main.js
@@ -2,7 +2,28 @@
 // INITIATION FUNCTION
 // ---------------------------------------------
 var init = function(){
-  new WOW().init();
+  // Initiate page animations
+  // new WOW().init();
+
+  // Anchor links scroll to anchor
+  $("a[href^='#']").on('click', function(e) {
+
+     // prevent default anchor click behavior
+     e.preventDefault();
+
+     // store hash
+     var hash = this.hash;
+
+     // animate
+     $('html, body').animate({
+         scrollTop: $(hash).offset().top
+       }, 300, function(){
+
+         // when done, add hash to url (default click behaviour)
+         window.location.hash = hash;
+       });
+
+  });
 }
 
 
