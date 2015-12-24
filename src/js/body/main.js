fe024c1568723d71d0f4877fee816caa072d6d5c
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2015-12-24 22:10:49 +0100

    Anchor menu links fix + minor tweaks

--- src/js/body/main.js
+++ src/js/body/main.js
@@ -2,27 +2,23 @@
 // INITIATION FUNCTION
 // ---------------------------------------------
 var init = function(){
-  // Initiate page animations
-  // new WOW().init();
 
-  // Anchor links scroll to anchor
-  $("a[href^='#']").on('click', function(e) {
-
-     // prevent default anchor click behavior
-     e.preventDefault();
-
-     // store hash
-     var hash = this.hash;
-
-     // animate
+  // Anchor links animate scroll
+  $("a[href^='#']").click(function(e) {
+     e.preventDefault(); // prevent default anchor click behavior
+     var hash = this.hash; // store hash
+     // animate scroll
      $('html, body').animate({
          scrollTop: $(hash).offset().top
        }, 300, function(){
-
          // when done, add hash to url (default click behaviour)
          window.location.hash = hash;
        });
+  });
 
+  // Collapse menu on click on anchor links in navbar
+  $(".navbar-nav li a[href^='#']").click(function(e) {
+    $(".navbar-collapse").collapse("hide");
   });
 }
 
