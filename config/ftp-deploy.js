fc697cd89721167d7f18ec242347eb3dd39aa0e4
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2015-12-28 13:22:36 +0100

    Grunt deploy task update + small fixes

--- config/ftp-deploy.js
+++ config/ftp-deploy.js
@@ -4,10 +4,10 @@ module.exports = function(grunt, options){
       auth: {
         host: grunt.option("deployDomain"),
         port: 21,
-        authKey: 'key1'
+        authKey: 'test'
       },
       src: 'build/<%= grunt.option("deploySubDir") %>',
-      dest: 'root/<%= grunt.option("deploySubDir") %>',
+      dest: '<%= grunt.option("deploySubDir") %>',
       exclusions: ['build/<%= grunt.option("deploySubDir") %>**/.DS_Store',
                    'build/<%= grunt.option("deploySubDir") %>**/Thumbs.db'],
       forceVerbose: true
