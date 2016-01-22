eda29e8c47469284a37778ed34e681cc0f9e1a98
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2016-01-22 20:08:30 +0100

    Personal site first commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function (grunt) {
   //----------------------------------------------
   //- Global variables
   //----------------------------------------------
-  var deployDomain = 'yourwebsite.com'; // No trailing slash, no subdomain
+  var deployDomain = 'wardoosterlijnck.be'; // No trailing slash, no subdomain
   var deploySubDir = ''; // Trailing slash
   var deployURL = deployDomain + '/' + deploySubDir;
 
