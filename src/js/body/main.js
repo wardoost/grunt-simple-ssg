2fa6a5a1486be00692f0c648d355045b95d87d94
Ward Oosterlijnck <wardoosterlijnck@gmail.com>
2016-02-11 19:25:14 +0100

    SPAM proofing mailto links with JS

--- src/js/body/main.js
+++ src/js/body/main.js
@@ -2,9 +2,16 @@
 var init = function(){
 
   //- Google analytics event for outbound links
-  $("a[href^='http']").click(function(e) {
+  $("a[href^='http']").on("click", function(e) {
     var url = $(this).attr("href");
     ga('send', 'event', 'outbound', 'click', url, { 'transport': 'beacon' });
+  });
+
+  //- Email link
+  $("#mail").on("click", function(e) {
+    var username = "wardoosterlijnck"
+    var domain = "gmail.com";
+    window.location.href = "mailto:" + username + "@" + domain;
   });
 }
 
